0852ff2ba08d687ec1b3d410db7258c103d1ae4a
ydh6226 <ydh6226@naver.com>
2023-01-23 20:47:41 +0900

    프로젝트 생성

---
+++ src/test/kotlin/com/example/apicomposition/ApiCompositionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apicomposition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiCompositionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
